65b49251df7f3f142b431b468d3cc38d4bf6bcb5
Dmitry Gusev <dmitry.gusev.95@gmail.com>
2017-07-20 15:25:42 +0300

    Add files via upload
    The first task about strings

--- task04/src/java/Main.java
+++ task04/src/java/Main.java
@@ -1,1 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printEvenLetters();
+    }
+
+    void printEvenLetters() {
+        String word = "";
+        StringBuilder wordBuilder = new StringBuilder();
+        System.out.println("Enter your word:");
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in))) {
+            word = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (i % 2 != 1) wordBuilder.append(word.charAt(i));
+        }
+        System.out.println(wordBuilder);
+    }
+}
