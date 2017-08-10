d171d7833c7601eb7154b783ed23e8559b4d2032
Dmitry Gusev <dmitry.gusev.95@gmail.com>
2017-08-10 16:54:30 +0300

    Added HW with Thread's join(), wait() and yield()

--- threadsMethods/Yield.java
+++ threadsMethods/Yield.java
@@ -1,1 +1,18 @@
+public class Yield {
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.err.println(Thread.currentThread().getName() + " " + i);
+                    Thread.yield();
+                }
+            }
+        };
 
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+    }
+}
